5ea4c3779a8426f02a660e9d324302de5b1c307f
Paul Annesley <paul@annesley.cc>
2012-01-14 16:22:33 +1100

    Building are infinitely generated! No garbage collection yet.

--- asc.rb
+++ asc.rb
@@ -2,13 +2,13 @@
 
 class Game
   def initialize
-    @world = World.new
+    @world = World.new(120)
     @screen = Screen.new(100, 40, @world)
   end
   def run
     loop do
-      render
       @world.tick
+      render
     end
   end
   def render
@@ -30,16 +30,16 @@ class Screen < Struct.new(:width, :height, :world)
     @fb = Framebuffer.new
   end
   def draw renderable
-    (renderable.y..(renderable.y + renderable.height)).each do |y|
-      (renderable.x..(renderable.x + renderable.width)).each do |x|
+    (renderable.y...(renderable.y + renderable.height)).each do |y|
+      (renderable.x...(renderable.x + renderable.width)).each do |x|
         @fb.set x, y, renderable.char
       end
     end
   end
   def render
     print "\e[H"
-    (0..height).each do |y|
-      (OFFSET..(width - OFFSET)).each do |x|
+    (0...height).each do |y|
+      (OFFSET...(width - OFFSET)).each do |x|
         print @fb.get(x, y)
       end
       print "\n"
@@ -61,28 +61,51 @@ class Framebuffer
 end
 
 class World
-  def initialize
+  def initialize horizon
+    @horizon = horizon
+    @building_generator = BuildingGenerator.new(self)
     @player = Player.new(25)
     @buildings = []
     [
       [-10, 30, 20],
       [20, 35, 20],
-      [50, 20, 20],
     ].each do |params|
       @buildings << Building.new(*params)
     end
   end
-  attr_reader :buildings, :player
+  attr_reader :buildings, :player, :horizon
   def tick
+    @building_generator.generate_if_necessary
     buildings.each do |b|
       b.x -= 1
     end
   end
 end
 
+class BuildingGenerator < Struct.new(:world)
+  def generate_if_necessary
+    while (b = world.buildings.last).x < world.horizon
+      world.buildings << Building.new(
+        b.right_x + minimium_gap + rand(8),
+        next_y(b),
+        rand(8) + 8
+      )
+    end
+  end
+  def minimium_gap; 4 end
+  def maximum_height_delta; 5 end
+  def minimum_height_clearance; 10; end
+  def next_y previous_building
+    p = previous_building
+    delta = maximum_height_delta * -1 + rand(2 * maximum_height_delta + 1)
+    [40, [previous_building.y - delta, minimum_height_clearance].max].min
+  end
+end
+
 class Building < Struct.new(:x, :y, :width)
-  def height; 30 end
+  def height; 50 end
   def char; "#" end
+  def right_x; x + width end
 end
 
 class Player < Struct.new(:y)
